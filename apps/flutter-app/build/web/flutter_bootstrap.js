d505df9ab8c5c609154874ab95da1cc1f91f9be1
ANITA CHAHAR <85778064+adityapro11@users.noreply.github.com>
2026-05-17 07:06:04 +0000

    Web app update with setting screen

--- apps/flutter-app/build/web/flutter_bootstrap.js
+++ apps/flutter-app/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "521588914"
+    serviceWorkerVersion: "2814725044"
   }
 });
