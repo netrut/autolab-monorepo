72df02133aff2ece597179fdda8f848bccd76b77
ANITA CHAHAR <85778064+adityapro11@users.noreply.github.com>
2026-07-25 02:33:16 +0000

    Customer app wrong app pop-up updated

--- apps/customer_app/build/web/flutter_bootstrap.js
+++ apps/customer_app/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3339315010"
+    serviceWorkerVersion: "3806414368"
   }
 });
