011834756a05e54c6d199f54d02936c73793ea13
ANITA CHAHAR <85778064+adityapro11@users.noreply.github.com>
2026-07-25 01:38:32 +0000

    Web app re-build & publish

--- apps/flutter-app/build/web/flutter_bootstrap.js
+++ apps/flutter-app/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2820318276"
+    serviceWorkerVersion: "3691530177"
   }
 });
